254789a090365bc9ebeab140ddb4d109a6050395
AgrinP <n7toman@gmail.com>
2025-04-06 17:10:16 +0300

    feat(styles): add autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import Inspect from 'vite-plugin-inspect'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,12 @@ export default defineConfig({
         Inspect(),
         ViteImageOptimizer(),
     ],
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer({})
+            ],
+        }
+    },
+    base: '/chef-claude-react-app/'
 })
